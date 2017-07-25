c813bc1b43fc2d0cd1c9b17d5b5fbbb3027d7072
jonesja <jared.jones@cdk.com>
2017-07-25 14:39:45 -0500

    Adding styles & content to outer layer (nav and hero) and documentation outline menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { DocComponent } from './documentation/doc.component';
+
+const routes: Routes = [
+    { path: 'home', component: HomeComponent },
+    { path: 'documentation', component: DocComponent },
+    { path: '', redirectTo: '/home', pathMatch: 'full' }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DocComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes, {enableTracing: true})
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/documentation/doc.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'doc-component',
+  templateUrl: './doc.component.html',
+  styleUrls: ['../app.component.scss']
+})
+export class DocComponent {
+  title = 'app';
+}
